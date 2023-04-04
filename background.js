d90a599e3e2ad9237308fb0c40e4e1fb6ea2053f
Serdar Cevher <scevher@gmail.com>
2023-04-04 07:31:52 +0300

    Yeni domain'i destekle

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) { //kullanici ikona tikladiginda calisir
-    if (tab.url.indexOf("https://eksisozluk.com/") != -1) {
+    if (tab.url.indexOf("https://eksisozluk.com/") != -1 || tab.url.indexOf("https://eksisozluk2023.com/") != -1) {
 
         chrome.tabs.executeScript(tab.id, {
             "file": "favorileri_sirala.js"
