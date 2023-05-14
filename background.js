7dd221209e7e497ac4ba401b6e3dc6114dcfd1dd
Serdar Cevher <scevher@gmail.com>
2023-05-14 14:09:18 +0300

    Eksisozluk42.com alan adı desteğini ekle

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 chrome.browserAction.onClicked.addListener(function (tab) { //kullanici ikona tikladiginda calisir
-    if (tab.url.indexOf("https://eksisozluk.com/") != -1 || tab.url.indexOf("https://eksisozluk2023.com/") != -1) {
+    if (
+        tab.url.indexOf("https://eksisozluk.com/") != -1 || 
+        tab.url.indexOf("https://eksisozluk2023.com/") != -1 ||
+        tab.url.indexOf("https://eksisozluk42.com/") != -1)
+        {
 
         chrome.tabs.executeScript(tab.id, {
             "file": "favorileri_sirala.js"
