b01643426dc481e92cb069ef5f6ffc697a3d28ed
Serdar Cevher <scevher@gmail.com>
2024-11-16 18:38:25 +0300

    Background.js'yi browser agnostik yap. Firefox için güncellenmiş manifest dosyası ekle.

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
-chrome.action.onClicked.addListener(function (tab) { //kullanici ikona tikladiginda calisir
+const browserAPI = typeof browser !== 'undefined' ? browser : chrome;
+
+browserAPI.action.onClicked.addListener(function (tab) {
     if (
         tab.url.indexOf("https://eksisozluk.com/") != -1)
         {
-        chrome.scripting.executeScript({
+        browserAPI.scripting.executeScript({
             target: { tabId: tab.id },
             files: ['favorileri_sirala.js']
         });
