fc728e1c619122de5f92391e8a4cc25dfa78e0ca
Serdar Cevher <scevher@gmail.com>
2024-11-17 19:14:08 +0300

    jQuery bağımlılığını kaldır. Firefox için gecko id ekle.

--- background.js
+++ background.js
@@ -1,12 +1,7 @@
 const browserAPI = typeof browser !== 'undefined' ? browser : chrome;
 
 browserAPI.action.onClicked.addListener(function (tab) {
-    if (
-        tab.url.indexOf("https://eksisozluk.com/") != -1)
-        {
-        browserAPI.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ['favorileri_sirala.js']
-        });
+    if (tab.url.indexOf("https://eksisozluk.com/") != -1) {
+        browserAPI.tabs.sendMessage(tab.id, { action: "sortFavorites" });
     }
 });
--- favorileri_sirala.js
+++ favorileri_sirala.js
@@ -1,36 +1,67 @@
-jQuery(document).ready(function(){
+const favorileriSirala = () => {
+    // Entry listesini favorilere göre sırala
+    const list = document.getElementById('entry-item-list');
+    if (list) {
+        const listItems = Array.from(list.querySelectorAll('li[data-favorite-count]'));
+        listItems.sort((a, b) => {
+            return parseInt(b.getAttribute('data-favorite-count')) - parseInt(a.getAttribute('data-favorite-count'));
+        });
+        
+        // Mevcut elemanları kaldır
+        listItems.forEach(item => item.remove());
+        
+        // Sıralanmış elemanları ekle
+        listItems.forEach(item => list.appendChild(item));
+    }
 
-  var list = $('ul#entry-item-list');
-  var listItems = list.find('li[data-favorite-count]').sort(function(a,b){ return $(b).attr('data-favorite-count') - $(a).attr('data-favorite-count'); });
-  list.find('li[data-favorite-count]').remove();
-  list.append(listItems);
+    // Favori ikonlarını ekle
+    const favoritesExist = document.querySelector('ul#entry-list li:first-child span.favorite-links') !== null;
 
+    if (!favoritesExist) {
+        document.querySelectorAll('ul#entry-list li').forEach(li => {
+            const favoriteCount = li.getAttribute('data-favorite-count');
+            const rateOptions = li.querySelector('span.rate-options');
+            
+            if (rateOptions) {
+                rateOptions.insertAdjacentHTML(
+                    'afterend',
+                    `<span class="favorite-links">
+                        <a class="icon icon-damla"></a>
+                        <a class="favorite-count toggles" style="display: inline;">${favoriteCount}</a>
+                    </span>`
+                );
+            }
+        });
+    }
 
-  var favorites_exists = 0;
-  if ($('ul#entry-list li:first span.favorite-links').length) {
-    favorites_exists = 1;
-  }
+    // Başlık listesini entry sayısına göre sırala
+    const topicList = document.querySelector('ul.topic-list');
+    if (topicList) {
+        // Entry sayılarını data attribute olarak ekle
+        document.querySelectorAll('ul.topic-list li').forEach(li => {
+            const entryCountEl = li.querySelector('a small');
+            if (entryCountEl) {
+                const entryCount = parseInt(entryCountEl.textContent);
+                li.setAttribute('data-entry-count', entryCount);
+            }
+        });
 
-  if (favorites_exists == 0) {
+        // Listeyi sırala
+        const topicItems = Array.from(topicList.querySelectorAll('li[data-entry-count]'));
+        topicItems.sort((a, b) => {
+            return parseInt(b.getAttribute('data-entry-count')) - parseInt(a.getAttribute('data-entry-count'));
+        });
 
-    $.each( $('ul#entry-list li'), function() {
-      var favorite_count = $(this).attr('data-favorite-count');
-      $(this).find('span.rate-options').after('<span class="favorite-links"><a class="icon icon-damla"></a> <a class="favorite-count toggles" style="display: inline;">' + favorite_count + '</a></span>');
-    });
+        // Mevcut elemanları kaldır ve sıralı şekilde ekle
+        topicItems.forEach(item => item.remove());
+        topicItems.forEach(item => topicList.appendChild(item));
+    }
+};
 
-  }
+const browserAPI = typeof browser !== 'undefined' ? browser : chrome;
 
-
-  var topic_list = $('ul.topic-list');
-
-  $.each( $( "ul.topic-list li" ), function() {
-    var entry_count = parseInt($(this).find('a small').text());
-    $(this).attr('data-entry-count', entry_count);
-  });
-
-  var topicListItems = topic_list.find('li[data-entry-count]').sort(function(a,b){ return $(b).attr('data-entry-count') - $(a).attr('data-entry-count'); });
-  topic_list.find('li[data-entry-count]').remove();
-  topic_list.append(topicListItems);
-
-
+browserAPI.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "sortFavorites") {
+        favorileriSirala();
+    }
 });
