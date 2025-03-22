050bf87c6509dede646c3b2933bcf535d1c79c79
Serdar Cevher <scevher@gmail.com>
2025-03-22 13:03:26 +0300

    Chrome'da scriptin çalışmasını sağla

--- background.js
+++ background.js
@@ -1,7 +1,24 @@
-const browserAPI = typeof browser !== 'undefined' ? browser : chrome;
+const browserAPI = typeof browser !== "undefined" ? browser : chrome;
 
 browserAPI.action.onClicked.addListener(function (tab) {
-    if (tab.url.indexOf("https://eksisozluk.com/") != -1) {
-        browserAPI.tabs.sendMessage(tab.id, { action: "sortFavorites" });
-    }
+  if (tab.url.indexOf("https://eksisozluk.com/") != -1) {
+    browserAPI.tabs.sendMessage(tab.id, { action: "sortFavorites" });
+  }
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Eklenti yüklendi");
+});
+
+// Content script'lerin yüklenmesini sağla
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (
+    changeInfo.status === "complete" &&
+    tab.url?.startsWith("https://eksisozluk.com")
+  ) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ["script.js"],
+    });
+  }
 });
